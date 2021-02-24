d7444e14e28753b7d2b5794870716524e9d8894a
dylan-albertazzi <albertad@oregonstate.edu>
2021-02-24 15:11:13 -0800

    push

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
 import ButtonGroup from '../elements/ButtonGroup';
 import Button from '../elements/Button';
 import Image from '../elements/Image';
 import Modal from '../elements/Modal';
+import rocketDoge from '../../assets/images/doge-rocket.jpeg';
+
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import awsExports from '../../aws-exports';
+
+import { updateDoge } from '../../graphql/mutations'
+import { getDoge } from '../../graphql/queries'
+Amplify.configure(awsExports);
 
 const propTypes = {
   ...SectionProps.types
@@ -25,6 +33,10 @@ const Hero = ({
   ...props
 }) => {
 
+  useEffect(() => {
+    fetchDogePrice()
+  }, [])
+
   const [videoModalActive, setVideomodalactive] = useState(false);
 
   const openModal = (e) => {
@@ -36,6 +48,43 @@ const Hero = ({
     e.preventDefault();
     setVideomodalactive(false);
   }   
+  const [dogePrice, setDogePrice] = useState(0);
+
+  async function fetchDogePrice() {
+    try {
+      console.log("in doge price")
+      const dogeData = await API.graphql(graphqlOperation(getDoge))
+      const dogePrice = dogeData.data.getDoge.price
+      console.log("==Dogeprice: ", dogePrice)
+      setDogePrice(dogePrice)
+
+    } catch (err) {
+      console.log('error fetching todos')
+      console.log(err)
+    }
+  }
+
+  async function updateDogePrice() {
+    try {
+      console.log("in doge price")
+      const dogeData = await API.graphql(graphqlOperation(getDoge))
+      const dogePrice = dogeData.data.getDoge.price + 0.1
+      console.log("==Dogeprice: ", dogePrice)
+      // const dogeObject = { id: "1", price: dogePrice }
+      const updatedDogePrice = await API.graphql(graphqlOperation(updateDoge, { input: dogePrice }))
+      console.log("==Dogeprice: ", updatedDogePrice)
+      setDogePrice(updatedDogePrice.data.updateDoge.price)
+      // var snd = new Audio("../../assets/images/cash.mp3"); // buffers automatically when created
+      // snd.play();
+      const audioEl = document.getElementsByClassName("audio-element")[0]
+      audioEl.play()
+
+    } catch (err) {
+      console.log('error fetching todos')
+      console.log(err)
+    }
+  }
+
 
   const outerClasses = classNames(
     'hero section center-content',
@@ -61,48 +110,47 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Dogecoin Price <span className="text-color-primary">Predictor</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                One click = 10 cents
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
+                <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+               <span className="text-color-secondary">$ {dogePrice.toFixed(2)}</span>
+               </h1>
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
+                  <Button tag="a" color="secondary" wideMobile onClick={updateDogePrice}>
+                    Doge 🚀
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
-                    </Button>
+                  
                 </ButtonGroup>
               </div>
             </div>
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
             <a
-              data-video="https://player.vimeo.com/video/174002812"
-              href="#0"
+              
               aria-controls="video-modal"
               onClick={openModal}
             >
               <Image
                 className="has-shadow"
-                src={require('./../../assets/images/video-placeholder.jpg')}
+                src={rocketDoge}
                 alt="Hero"
                 width={896}
                 height={504} />
             </a>
           </div>
-          <Modal
-            id="video-modal"
-            show={videoModalActive}
-            handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
-            videoTag="iframe" />
+          
         </div>
       </div>
+      <audio className="audio-element">
+        <source src={require("../../assets/images/cash.mp3")}></source>
+      </audio>
     </section>
+    
   );
 }
 
